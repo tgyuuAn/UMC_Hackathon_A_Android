82409cba0219edc6022f473fee039db8672cdd65
choiminjun <68235719+mj010504@users.noreply.github.com>
2023-11-24 19:07:30 +0900

    feature : AuthFragment 베이스 작업

---
+++ app/src/main/java/com/pknu/busannollerwar/presentation/auth/AuthViewModel.kt
@@ -0,0 +1,18 @@
+package com.pknu.busannollerwar.presentation.auth
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.pknu.busannollerwar.presentation.home.HomeEvent
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel()
+class AuthViewModel @Inject constructor() : ViewModel() {
+    private val _eventFlow = MutableSharedFlow<AuthEvent>()
+    val eventFlow get() = _eventFlow.asSharedFlow()
+
+    private fun event(event: AuthEvent) = viewModelScope.launch { _eventFlow.emit(event) }
+}
