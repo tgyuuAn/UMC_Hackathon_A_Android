60e833da2a2558441121e00fbacca01936eadf39
tgyuu-An <atg0614@gmail.com>
2023-11-24 14:49:13 +0900

    feature : BottomNavigationView Visibilty 로직 추가

--- app/src/main/java/com/pknu/busannollerwar/MainActivity.kt
+++ app/src/main/java/com/pknu/busannollerwar/MainActivity.kt
@@ -2,6 +2,8 @@ package com.pknu.busannollerwar
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.pknu.busannollerwar.databinding.ActivityMainBinding
@@ -9,6 +11,7 @@ import com.pknu.busannollerwar.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+    lateinit var navController : NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +25,27 @@ class MainActivity : AppCompatActivity() {
     private fun setBottomNavigationView() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.fcv_main_fragmentContainverView) as NavHostFragment
-        val navController = navHostFragment.navController
+        navController = navHostFragment.navController
         NavigationUI.setupWithNavController(binding.bnvMainBottomNaviView, navController)
+
+        setBottomNavigationVisiblity()
+    }
+
+    private fun setBottomNavigationVisiblity() {
+        val hideBottomNavigationFragments = mutableListOf<Int>()
+
+        val typedArray = resources.obtainTypedArray(R.array.hide_bottomNavigation_fragments)
+
+        for (index in 0..typedArray.length()) {
+            hideBottomNavigationFragments.add(typedArray.getResourceId(index, 0))
+        }
+
+        typedArray.recycle()
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+
+            binding.bnvMainBottomNaviView.isVisible =
+                (destination.id !in hideBottomNavigationFragments)
+        }
     }
 }
