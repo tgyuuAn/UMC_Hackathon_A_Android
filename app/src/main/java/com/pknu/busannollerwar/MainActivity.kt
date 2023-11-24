4934108e200f85b44feda9f523a3a818e1d06170
tgyuu-An <atg0614@gmail.com>
2023-11-24 14:23:06 +0900

    feautre : bottomNavigationView 기초 작업

--- app/src/main/java/com/pknu/busannollerwar/MainActivity.kt
+++ app/src/main/java/com/pknu/busannollerwar/MainActivity.kt
@@ -2,6 +2,8 @@ package com.pknu.busannollerwar
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.pknu.busannollerwar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,7 +15,14 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        
+        setBottomNavigationView()
+    }
 
-
+    private fun setBottomNavigationView() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fcv_main_fragmentContainverView) as NavHostFragment
+        val navController = navHostFragment.navController
+        NavigationUI.setupWithNavController(binding.bnvMainBottomNaviView, navController)
     }
 }
