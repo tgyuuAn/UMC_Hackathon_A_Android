a2c54e509c5c7cf34b34f1a7dfe23f978588a4bd
tgyuu-An <atg0614@gmail.com>
2023-11-25 02:19:25 +0900

    feature : contents Detail

---
+++ app/src/main/java/com/pknu/busannollerwar/presentation/home/contents/ContentsFragment.kt
@@ -0,0 +1,14 @@
+package com.pknu.busannollerwar.presentation.home.contents
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.pknu.busannollerwar.R
+import com.pknu.busannollerwar.databinding.FragmentContentsBinding
+import com.pknu.busannollerwar.presentation.util.BaseFragment
+
+class ContentsFragment : BaseFragment<FragmentContenstBinding, ContentsViewModel>(
+    FragmentContentsBinding::inflate) {
+}
